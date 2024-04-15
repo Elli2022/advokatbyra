121d2765620c295128ccb03a9e7d04a67ce45519
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-15 15:13:54 +0200

    fixed Navbar.tsx and LargeBanner.tsx

--- src/components/LargeBanner.tsx
+++ src/components/LargeBanner.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import lawFirmBanner from "../images/lawFirmBanner.jpg";
-import TextInsideBanner from "./TextInsideBanner";
+// import TextInsideBanner from "./TextInsideBanner";
+import { Link } from "react-router-dom";
 
 function LargeBanner() {
   return (
@@ -28,47 +30,33 @@ function LargeBanner() {
           transform: "translate(-50%, -50%)",
           color: "white",
           textAlign: "left",
-          backgroundColor: "rgba(0, 0, 0, 0)", // Lätt genomskinlig svart bakgrund för att förbättra textläsbarheten
+          backgroundColor: "rgba(0, 0, 0, 0)", 
         }}
       >
         <h2
           style={{
             backgroundColor: "rgba(0, 0, 0, 0)",
             color: "white",
-            display: "flex",
-            fontFamily: "adobe-caslon-pro,times,georgia,serif",
-            fontSize: "calc(56px + 0.5vw)", // Exempel på responsiv fontstorlek
-            maxWidth: "800px", // Maxbredd för textbehållaren
-            lineHeight: "1.6", // Optimal line-height för läsbarhet
-            margin: "26px auto", // Centrera behållaren horisontellt med 'auto'
-            padding: "10px", // Utrymme runt texten
+            position: "relative",
+            // top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "left",
+            fontFamily: "Caslon224Std-Book,times,georgia,serif",
+            fontWeight: 300,
+            lineHeight: 1.2,
+            fontSize: "34px",
           }}
         >
-          Welcome to Our Website
+          Välkommen till W Advokatbyrå
         </h2>
-        <div style={{ backgroundColor: "transparent" }}>
-          <button
-            style={{
-              backgroundColor: "transparent",
-              border: "1px solid white",
-              color: "white",
-              padding: "15px 32px",
-              textAlign: "center",
-              textDecoration: "none",
-              display: "inline-block",
-              fontSize: "16px",
-              fontFamily: "MonumentGrotesk-Regular,helvetica,arial,sans-serif",
-              letterSpacing: ".5px",
-              margin: "4px 2px",
-              minWidth: "246px",
-              transitionDuration: "0.4s",
-              cursor: "pointer",
-              borderRadius: "0",
-            }}
-            onClick={() => alert("Button clicked!")} // Exempelhändelse när knappen klickas
-          >
+        <div
+          className="föräldrarelement"
+          style={{ backgroundColor: "transparent" }}
+        >
+          <Link to="/about" className="buttonStyle">
             Om oss
-          </button>
+          </Link>
         </div>
       </div>
     </div>
