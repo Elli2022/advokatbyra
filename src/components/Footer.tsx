e2cf42fc3c9610395e6762189848c1f86b2025a7
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-05-18 19:59:16 +0200

    Fixed Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Container from "./Container";
 import instagramIcon from "../images/instagramIcon.png";
 import facebookIcon from "../images/facebookIcon.png";
@@ -9,57 +8,57 @@ function Footer() {
   return (
     <Container>
       <footer className="footer">
-        <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px" }}>
           <p
             style={{
               fontFamily: "MonumentGrotesk-Regular,helvetica,arial,sans-serif",
-              display: "flex",
               fontSize: "calc(13px + 0.5vw)",
               textAlign: "center",
+              margin: "0px"
             }}
           >
-            2021-2023 © Wernberg Advokatbyrå AB med säte i Malmö
+            2021-2023 © W Advokatbyrå AB med säte i Malmö
           </p>
-
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ border: "none", outline: "none", display: "inline-block" }}
-          >
-            <img
-              src={instagramIcon}
-              alt="Instagram Icon"
-              style={{ height: "50px", width: "60px", borderRadius: "5px" }}
-            />
-          </a>
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ border: "none", outline: "none", display: "inline-block" }}
-          >
-            <img
-              src={facebookIcon}
-              alt="Facebook Icon"
-              style={{ height: "50px", width: "50px", borderRadius: "5px" }}
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ border: "none", outline: "none", display: "inline-block" }}
-          >
-            <img
-              src={linkedInIcon}
-              alt="LinkedIn Icon"
-              style={{ height: "36px", width: "36px", borderRadius: "10px" }}
-            />
-          </a>
-          <Link to="/disclaimer" style={{ color: "grey" }}>
-            DISCLAIMER
-          </Link>
+          <div style={{ display: "flex", justifyContent: "center", gap: "20px", alignItems: "center" }}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ display: "inline-block" }}
+            >
+              <img
+                src={instagramIcon}
+                alt="Instagram Icon"
+                style={{ height: "40px", width: "auto", borderRadius: "5px" }}
+              />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ display: "inline-block" }}
+            >
+              <img
+                src={facebookIcon}
+                alt="Facebook Icon"
+                style={{ height: "40px", width: "auto", borderRadius: "5px" }}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ display: "inline-block" }}
+            >
+              <img
+                src={linkedInIcon}
+                alt="LinkedIn Icon"
+                // Minska LinkedIn-ikonens höjd för att göra den mindre än de andra
+                style={{ height: "35px", width: "auto", borderRadius: "5px" }}
+              />
+            </a>
+            <Link to="/disclaimer" style={{ color: "grey", alignSelf: "center" }}>DISCLAIMER</Link>
+          </div>
         </div>
       </footer>
     </Container>
